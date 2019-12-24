f02f4e3c087ffc9f5f3ae47568bfd2320335c6b1
Your Actual Name <Your Actual Email>
2019-12-24 13:03:40 -0500

    framework and fundation is set

--- public/jQuery/curtainScreen.js
+++ public/jQuery/curtainScreen.js
@@ -27,3 +27,45 @@ $(document).ready(function() {
       }
     });
   });
+
+  //for expanding the list items HOME, ABOUT etc.
+  $(document).ready(function(){
+    $("#button-1").click(function(){
+      // $("listItem").css("fontSize", "100px");
+      // $( "h1" ).height( "50px" );
+      $("#special1").toggle();
+      //$( "li" ).height( "50px" );
+      // var text_input = $('#button-1');
+      // text_input.css("font-size", "50px");
+      $("#listItem").animate({
+        fontSize: $('#listItem').css('font-size') == '35px' ? '50px' : '35px'
+      });
+    })
+    
+    $("#button-2").click(function(){
+      $('#special2').toggle();
+      $('#listItem2').animate({
+        fontSize: $('#listItem2').css('font-size') == '35px' ? '50px' : '35px'
+      });
+    });
+    $("#button-3").click(function(){
+      $('#special3').toggle();
+      $('#listItem3').animate({
+        fontSize: $('#listItem3').css('font-size') == '35px' ? '50px' : '35px'
+      });
+    });
+    
+      // $("#listItem2").animate({
+      //   fontSize: $('#listItem2').css('font-size') == '35px' ? '50px' : '35px'
+      // });
+      // $("#listItem3").animate({
+      //   fontSize: $('#listItem3').css('font-size') == '35px' ? '50px' : '35px'
+      // });
+
+     
+      
+      // $("#btn2").click(function(){
+      //   $("#box").animate({height: "100px"});
+      // });
+  
+})
